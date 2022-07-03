c56aea86b8654863b17950bb8f1e8a823224a1eb
--get <67471674+ninebolt6@users.noreply.github.com>
2022-07-03 22:08:12 +0900

    fix: trailing commaを削除

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Lectures` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '講義ID',
   `name` varchar(96) NOT NULL COMMENT '講義名',
   `teacher` varchar(32) DEFAULT NULL COMMENT '教員名',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='講義情報';
 
 
@@ -17,5 +17,5 @@ CREATE TABLE `Timetables` (
   `day_of_week` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '開講曜日',
   `period` int NOT NULL COMMENT '開講時限',
   `memo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'メモ',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='時間割情報';
