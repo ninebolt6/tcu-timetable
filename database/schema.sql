358a1054000909b5c7e366422f50bf24f6798779
--get <67471674+ninebolt6@users.noreply.github.com>
2022-07-03 21:37:41 +0900

    chore: 教室テーブルを削除

--- database/schema.sql
+++ database/schema.sql
@@ -8,22 +8,12 @@ CREATE TABLE `Lectures` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='講義情報';
 
 
--- syllabus.Classrooms definition
-
-CREATE TABLE `Classrooms` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '教室ID',
-  `campus_id` int NOT NULL COMMENT 'キャンパス',
-  `name` varchar(100) NOT NULL COMMENT '教室名',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='教室情報';
-
-
 -- syllabus.Timetables definition
 
 CREATE TABLE `Timetables` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '時間割ID',
   `lecture_id` int NOT NULL COMMENT '講義ID',
-  `classroom_id` int DEFAULT NULL COMMENT '教室ID',
+  `classroom` varchar(64) DEFAULT NULL COMMENT '教室',
   `day_of_week` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '開講曜日',
   `period` int NOT NULL COMMENT '開講時限',
   `memo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'メモ',
