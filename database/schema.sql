a754ea2fa7e6f43ac63086c6cec2d8bb8354a7fa
--get <67471674+ninebolt6@users.noreply.github.com>
2022-07-04 20:49:35 +0900

    chore: 中間テーブル追加に伴いカラム削除

--- database/schema.sql
+++ database/schema.sql
@@ -12,11 +12,8 @@ CREATE TABLE `Lectures` (
 
 CREATE TABLE `Timetables` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '時間割ID',
-  `lecture_id` int NOT NULL COMMENT '講義ID',
-  `classroom` varchar(64) DEFAULT NULL COMMENT '教室',
-  `day_of_week` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '開講曜日',
-  `period` int NOT NULL COMMENT '開講時限',
-  `memo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'メモ',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '時間割名',
+  `memo` varchar(100) DEFAULT NULL COMMENT 'メモ',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='時間割情報';
 
